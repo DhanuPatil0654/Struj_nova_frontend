8fbdd1b853152b9f9a7777029f00e5284adfa7fc
DhanuPatil0654 <dhananjaypatil572001@gmail.com>
2025-11-19 00:23:13 +0530

    Add Google verification file

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-//   server: {
-//     host: true
-//   }
-// })
-
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { createHtmlPlugin } from "vite-plugin-html";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      minify: true,
-      inject: {
-        data: {
-          title: "Struj Nova – EV Charging Solutions",
-          description: "Best EV charging infrastructure & fast charger manufacturing",
-        },
-      },
-    }),
-  ],
+  plugins: [react()],
 });
